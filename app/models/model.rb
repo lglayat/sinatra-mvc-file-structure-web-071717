9e3c19daa31e0d9aabf846050572baecc957ddd2
lglayat <glayatle@g.cofc.edu>
2017-08-07 17:35:11 -0400

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,17 @@
-class Model
+class Dog
+
+  @@all = []
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
